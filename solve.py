d08441abaf4d58873a3274854f3ae314fd9d1307
lakelandcol <thethief45@yahoo.com>
2014-03-30 14:32:34 -0400

    power_rule takes the results and populates a new list; the elements in sys.argv are summed; work on summing elements of new list

--- solve.py
+++ solve.py
@@ -1,10 +1,12 @@
 #Written By Collin Lakeland
+
 #Program to solve any equation using Newton's Method
 
 
 #division module makes it so division operator doesn't round the 
 #quotient
 from __future__ import division
+
 #cmath module allows for complex solutions, may or may not be necessary
 #at this point
 import cmath
@@ -13,118 +15,140 @@ import sys
 class Solver:
 
 	def __init__(self, argv):
-		#User input
-		self.guess = int()
+		print ("Hello, welcome to Collin's solver program!\n"
+		"Note your equation must be set equal to zero,\nand your guess "
+		"must be fairly close to the root.")
+		
+		#User input; converted input to integer.
+		self.guess = int(input("Take a guess: "))
+		#Initial coefficients and powers.
 		self.args = argv
 		#Debigging
 		print self.args
-		#print self.args.index('1')
-		#print self.args.index('2')
-		#print self.args.index('3')
 		
 	#power_rule method is used on coeffcient/power pairs, ex. elements 
-	#at (1,2),(3,4), etc
+	#at (1,2),(3,4), etc.
 	
 	def power_rule(self):
+		#For results of power_rule. o is initially a placeholder.
+		self.o = 'solver.py'
+		new_list = []
 		x = 1
-		#Power elements
-		n = 2*x
+		#initial n value
+		n = 2
 		#Subtract initial argument, the program name, and divide it 
 		#by 2 to get the correct iterations
-		argcounter = (len(self.args)-1)/2
+		argcounter = (len(self.args) - 1) / 2
 		
-		for arg in self.args:
-			
-			print "p is %d" % int(self.args[n])
-			print "c is %d" % int(self.args[n-1])
-			print "n is", n
-			if argcounter == 0:
-				
-				break
-			#power * corresponding coefficient ^ (power - 1)
-			#convert list elements to integers
-			#TODO: If I try and break this line it errors
-			o = int(self.args[n])*int(self.args[n-1])**int(self.args[n]) - 1
-			x += 1
-			argcounter -= 1
-		
-			print "argcounter is %d" % argcounter
-		print "result is %d" % o
-		print self.args.index('3')
-		print self.args.index('1')
-
-		#Useless code and comments, at the moment.
-		
-		"""
-		numbers = []
-		c = 1
-		x = 1
-		#subtract 0th argument
-		argtotal = len(sys.argv)-1
-		#User enters even number of arguments
-		#argcounter is also the number of iterations needed for correct 
-		#coeffcient/power placement
-		final_iteration = argtotal/2
-		initial_iteration = 0
-		#print argtotal, argcounter
-
-		print "initial coef place is %d\n" % c
-		while final_iteration > initial_iteration:
-			for arg in self.args:
-
-				#power
-				p = 2*x
-				#coefficient
-				x = x + 1
-				initial_iteration = initial_iteration + 1
-				#Debugging
-				print "power place is ", p
-				print "x is", x
-				print "iteration = ", initial_iteration
-				#coefficient
-				c = p + 1
-				#if the coeff place is > the total arguments break the 
-				#for loop, occurs when final_iteration = 
-				#initial_iteration, so the while loop is also broken
-				if c > argtotal:
-					break
-				print "coeff place is %d\n" % c
-				
-				break
-				
-				FIX: Doesn't print out an extra coeffcient place, but 
-				the assignment is still taking place
-		
-				FIX: convert list arguments to integers maybe using for
-				loop
-				
-		MAYBE INSERT A 0 AT THE BEGINNING OF SELF.ARGS TO GET CORRECT
-		INDEXING AND PLACEMENT?
-		
-		MAYBE USE THIS?
+		while argcounter >= 0:
 	
-		n = 0
-		print self.args
-		print len(self.args)
-		for args in self.args:
-			if n > len(self.args):
+			new_list.append(self.o)
+			#Stop appending once argcounter = 0.
+			if argcounter == 0:
 				break
-			numbers.append(self.args[n])
-			n = n + 1
-				"""
+			for arg in self.args:
+			
+				print "p is %d" % int(self.args[n])
+				print "c is %d" % int(self.args[n-1])
+				print "x is %d" % x
+				print "n is %d" % n
+				
+				#power * (corresponding coefficient * guess ^ (power - 1))
+				#convert list elements to integers
+				self.o = float(self.args[n]) * ( float(self.args[n-1]) * 
+				self.guess ** ( float(self.args[n]) - 1 ) ) 
+				
+				x += 1
+				#Power elements
+				n = 2*x
+				argcounter -= 1
+				#%f is the string formatter for REAL, floating numbers. This may cause problems later on with complex solutions.
+				print "argcounter is %f" % argcounter
+				print "result is %f" % self.o
+				#When  the result is computed, break the for loop, go
+				#back to the while loop and append each result into
+				#new_list.
+				break
+		#TODO: string formatting for lists?
+		print "new_list is", new_list
+		
+	#TODO: The creation of new_list should be another method, but I
+	#don't know how to do that, because the results (self.o) constantly
+	#get updated.
+	
+	#def new_coeff_power_list(self):
+		
+	
+	def Newtons_method(self):
+		#This sums all the elements in the initial list (argv).
+		a = 2
+		n = len(self.args) - 1
+		#Add the first two elements.
+		#float is used because you could have decimals as input
+		sum = float(self.args[a-1]) + float(self.args[a])
+		print "the initial sum is %f" % sum
+		while a <= n:
+			a += 1
+			#When a = n, a then = a+1, which is out of the list range. This prevents it from erroring
+			if a > n:
+				break
+			print "a is %d" % a
+			sum = sum + float(self.args[a])
+			print "the current sum is %f\n" % sum
+		print "The final sum is %f" % sum
+	
+
+#Useless code and comments, at the moment.
+	
+"""
+numbers = []
+c = 1
+x = 1
+#subtract 0th argument
+argtotal = len(sys.argv)-1
+#User enters even number of arguments
+#argcounter is also the number of iterations needed for correct
+#coeffcient/power placement
+final_iteration = argtotal/2
+initial_iteration = 0
+#print argtotal, argcounter
+
+print "initial coef place is %d\n" % c
+while final_iteration > initial_iteration:
+for arg in self.args:
+
+#power
+p = 2*x
+#coefficient
+x = x + 1
+initial_iteration = initial_iteration + 1
+#Debugging
+print "power place is ", p
+print "x is", x
+print "iteration = ", initial_iteration
+#coefficient
+c = p + 1
+#if the coeff place is > the total arguments break the 
+#for loop, occurs when final_iteration = 
+#initial_iteration, so the while loop is also broken
+if c > argtotal:
+break
+print "coeff place is %d\n" % c
+
+break
+
+n = 0
+print self.args
+print len(self.args)
+for args in self.args:
+if n > len(self.args):
+break
+numbers.append(self.args[n])
+n = n + 1
+"""
 solverObject = Solver(sys.argv)
 solverObject.power_rule()
-	
-	
-	#print guess
-	
-	#self.o = sys.argv[c]*sys.argv[p]
-	
-		
-	#def power_rule(self, coeffs, powers):
-
-	#def Newtons_method(self, equation, guess):
-	
+solverObject.Newtons_method()
 
 """
 
