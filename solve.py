48fdb08845eced3663e21b5b94c3b33cc9721122
lakelandcol <thethief45@yahoo.com>
2014-06-02 20:22:58 -0400

    I almost made work

--- solve.py
+++ solve.py
@@ -1,4 +1,4 @@
-#Damn you Kanon Written By Collin Lakeland.
+#Written By Collin Lakeland.
 
 #This is a program to solve any equation using Newton's Method.
 
@@ -10,7 +10,9 @@ import numbers
 #cmath module allows for complex operations and solutions
 import cmath
 import math
-import sympy
+#sympy module allows for symbolic manipulation, and differentiation of special
+#mathematical functions.
+from sympy import *
 import re
 import sys
 import time
@@ -32,6 +34,8 @@ class Solver:
 		self.args = argv
 		self.sum_call_counter = 1
 		self.power_rule_counter = 1
+		self.conjugate_return = int()
+		self.keyword = False
 		#Results of substitution
 		self.y = complex()
 		#Results of power_rule
@@ -44,7 +48,7 @@ class Solver:
 	def power_rule_substitution(self):
 		try:
 			print "POWER RULE OUTPUT\n"
-			print "THE LENGTH OF ARGV IS {0}".format( len(self.args) )
+			#print "THE LENGTH OF ARGV IS {0}".format( len(self.args) )
 			x = 1
 			#Initial index value
 			self.i = 2
@@ -59,7 +63,6 @@ class Solver:
 				if self.argcounter == 0:
 					break
 				for arg in self.args:
-					
 					#print "p is", complex(self.args[self.i])
 					#print "c is", complex(self.args[self.i-1])
 					#print "x is", x
@@ -89,13 +92,11 @@ class Solver:
 					self.i = 2*x
 					self.argcounter -= 1
 					#time.sleep(2)
-					print "argcounter is %d" % self.argcounter
+					#print "argcounter is %d" % self.argcounter
 					#When  the result is computed, break the for loop, go
 					#back to the while loop and append each result into
 					#new_list.
 					break
-			#print "Power rule list is", self.power_rule_list
-			#print "Substitution list is", self.list_without_power_rule_operation
 			
 			#This is supposed to prevent an infinite loop from occuring, while
 			#allowing self.power_rule_substitution to be called a second time.
@@ -110,46 +111,59 @@ class Solver:
 		except ValueError as e:
 			print 'The error is "{0}"'.format(e)
 			print self.args
-			self.keyword = False
-			i = 0
+			i = 1
 			#If any of the following keywords are found within argv use sympy 
 			#methods.
-			for elements in self.args:
+			#print len(self.args)
+			while i < len(self.args):
 				#The 'r' isn't necessary but it is commonly used to denote a
 				#regular expression pattern.
 				if re.search( r'sin|cos|tan|sec|csc|cot|ln|log|sqrt|e|'
-					'pi', elements):
+					'pi', self.args[i]):
 						self.keyword = True
-						print "The keyword \"{0}\" is here.".format(elements)
+						#print "The keyword \"{0}\" is here.".format(self.args[i])
 				else:
-					print "\"{0}\" is not a keyword.".format(elements)
+					print " \"{0}\" is not a keyword.".format(self.args[i])
 				i += 1
-				if i > len(self.args):
-					break
-	
-	def special_differentiation(self):
+	#Creates sympy object.
+	def expression_creator(self):
+		self.x = symbols('x')
 		if self.keyword == True:
-			i = 1
-			for elements in self.args:
+			x = 1
+			argcounter = ( (len(self.args)) - 1 )/ 2
+			self.expression_string = ""
+			while argcounter > 0:
+				i = 2*x
+				x += 1
 				#Creates the first part of a string using sympy syntax; it will
 				#soon be turned into a sympy object.
-				term1 = "{0}**{1}+".format(self.args[i], self.args[i+1])
-				#Move on to next coefficient/ power pair.
-				#i will always be odd, and the length of self.args
-				#will always be odd, so once i equals the length,
-				#term2 isn't needed.
-				i += 2
-				if i == len(self.args):
-					#Strip the "+" on the end of the string.
-					expression = expression.rstrip("+")
-					break
-				#Create a new term, using the new value of i
-				#CLEANER WAY TO DO THIS??
-				term2 = "{0}**{1}+".format(self.args[i], self.args[i+1])
-				
-				expression = term1 + term2
-			print expression
-	
+				term = "{0}**{1}+".format(self.args[i-1], self.args[i])
+				#Continuosly add terms to the string.
+				self.expression_string += term
+				#print self.expression_string
+				argcounter -= 1
+				#print argcounter
+
+		#Strip the "+" on the end of the string.
+		self.expression_string = self.expression_string.rstrip("+")
+		print self.expression_string
+		self.expr = sympify(self.expression_string)
+		
+	#Finds the derivative of the expression, and performs Newtons method on the
+	#expression 1001 times.
+	def special_Newtons_method(self):
+		derivative = diff(self.expr, self.x)
+		n = 1
+		while n <= 1000:
+			#"subs" substitutes in the initial self.guess for the deifned symbol
+			#x.
+			self.guess -= ( self.expr.subs(self.x, self.guess) /
+
+				( derivative.subs(self.x, self.guess) ) )
+			n += 1
+		print "The final result of the SPECIAL Newton\'s method is", self.guess
+
+
 	#This sums all the elements in list_without_power_rule_operation and
 	#sums all the elements in new_list.
 	def sum_method(self, data_list):
@@ -157,8 +171,6 @@ class Solver:
 		a = 2
 		n = len(data_list) - 1
 		print "LENGTH - 1 IS %d" % n
-		print "data list is", self.list_without_power_rule_operation
-		print "the second pass in is", self.power_rule_list
 		
 		#Add the first two elements.
 		#complex is used because you could have complex numbers as input and
@@ -187,9 +199,7 @@ class Solver:
 			print "sum_counter is even %d" % self.sum_call_counter
 			self.power_rule_list_sum = self.sum
 			self.sum_call_counter += 1
-		print "THE FINAL SUM IS \n {0}".format(self.sum)
-		print
-
+		print "THE FINAL SUM IS {0}\n".format(self.sum)
 
 	#Applys the mathematical process called "Newton's method"
 	#Finds the zeros correctly, but the guess MUST be near the zero. If two
@@ -210,74 +220,60 @@ class Solver:
 			#Reset the lists after an iteration of Newton's_method
 			self.power_rule_list = [0]
 			self.list_without_power_rule_operation = [0]
-			print "newton_counter IS %d" % n
-			print "The new self.guess is {0}".format(self.guess)
+			#print "newton_counter IS %d" % n
+			#print "The new self.guess is {0}".format(self.guess)
 			#time.sleep(5)
+			
 		#The single result of Newton's
-		print "THE FINAL RESULT OF NEWTON'S METHOD IS: {0}".format(self.guess)
-		print
-
-
-	#Uses the complex conjugate theorem: if a + b*j is a root, then a - b*j
-	#is also a root.
-	def complex_conjugate(self):
-		print "CONJUGATE OUTPUT:\n"
-		#Checks whether all of the coefficients are real, if not, this method
-		# a condition in number_of_roots, and a condition in answer_spitter
-		#doesn't apply.
-		#Default vale is 0.
-		self.conjugate_return = int()
-		self.i = 1
-		m = 0
-		self.argcounter = (len(self.args) - 1 )/2
-		while self.argcounter >= 0:
-			print "The coefficients are:{0} \n".format(self.args[self.i])
-			if complex(self.args[self.i]).imag != 0:
-				print "Not all coeffcients are real\n"
-				self.conjugate_return = 1
-				return 1
-			self.argcounter -= 1
-			if self.argcounter == 0:
-				break
-			m += 1
-			#self.i is always odd
-			self.i = 2*m + 1
-		print "All coeffcients are real."
+		print "THE FINAL RESULT OF NEWTON'S METHOD IS: {0}\n".format(self.guess)
 
 	#Tests whether or not the result of Newton's Method is an actual solution 
-	#to the equation, based on the user's guess by seeing how close the 
-	#equation is to zero If it isn't very close, then the user should try 
+	#to the equation, based on the user's guess, by seeing how close the 
+	#equation is to zero. If it isn't very close, then the user should try 
 	#another guess.
 	def solution_tester(self):
 		print "SOLUTION TESTER OUTPUT:\n"
-		print "The power_rule counter is", self.power_rule_counter
-		#As long as Newton's method is called 1001 times self.power_rule_counter 
-		#will always be 2005. Add 1 to it so when power_rule_substitution 
-		#is called the final self.guess is substituted in.
-		self.power_rule_counter += 1
-		print "Now the counter is {0}".format(self.power_rule_counter)
-		#Cleaner way of doing this?
-		self.list_without_power_rule_operation = [0]
-		self.power_rule_substitution()
-		self.sum_method(self.list_without_power_rule_operation)
-		print "The sum is {0}".format(self.sum)
-		#If both the real and imaginary parts of the sum are between a very
-		#small negative number and a very small positive number.
-		#e(number), in this case, is equivalent to 10**number.
-		if ( ( -1e-6 < self.sum.real < 1e-6 ) and
-		
-			( -1e-6 < self.sum.imag < 1e-6 ) ):
-		
-			print "VALID SOLUTION"			
+		if self.keyword == False:
+			#print "The power_rule counter is", self.power_rule_counter
+			#As long as Newton's method is called 1001 times
+			#self.power_rule_counter will always be 2005.
+			#Add 1 to it so when power_rule_substitution is called
+			#the final self.guess is substituted in.
+			self.power_rule_counter += 1
+			#print "Now the counter is {0}".format(self.power_rule_counter)
+			#Cleaner way of doing this?
+			self.list_without_power_rule_operation = [0]
+			self.power_rule_substitution()
+			self.sum_method(self.list_without_power_rule_operation)
+			#print "The sum is {0}".format(self.sum)
+			
+			#If both the real and imaginary parts of the sum are between a very
+			#small negative number and a very small positive number.
+			#e(number), in this case, is equivalent to 10**number.
+			if ( ( -1e-6 < self.sum.real < 1e-6 ) and
+			
+				( -1e-6 < self.sum.imag < 1e-6 ) ):
+			
+				print "VALID SOLUTION"			
+			else:
+				print ( "THE PROGRAM IS LYING TO YOU. Try another guess.\n"
+				"Maybe make it complex (e.g. of the form a+bj)" )
+				sys.exit()
+		#if there are keywords
+		elif -1e-6 < self.expr.subs(self.x, self.guess) < 1e-6:
+			print "VALID SOLUTION"
 		else:
 			print ( "THE PROGRAM IS LYING TO YOU. Try another guess.\n"
-			"Maybe make it complex (e.g. of the form a+bj)" )
+				"Maybe make it complex (e.g. of the form a+bj)" )
 			sys.exit()
 			
 	#IS THERE A CLEANER WAY OF WRITING THIS METHOD?
 	#If both parts of the solution or one part are tiny, then they default to 
 	#zero.
 	def approximate(self):
+		#To ensure self.guess has both real and imaginary parts, even if they
+		#are zero.
+		self.guess = complex(self.guess)
 		if ( -1e-6 < (self.guess.real) < 1e-6 and 
 			-1e-6 < (self.guess.imag) < 1e-6 ):
 			
@@ -302,52 +298,60 @@ class Solver:
 		self.sum_method(self.power_rule_list)
 
 	def reprompt(self):
-		#Initially, subtract 1 from the number of solutions, because the length 
-		#of self.answers is less than or equal to 1, and at this point one
-		#solution has been found.
-		
-		#self.solutions -= 1
 		print "PRE-REPROMPT OUTPUT:\n"
 		self.reprompt_return_value = 0
-		while self.solutions > 0:
-			if self.answers.count(self.guess) == 0:
-				print "That is a new solution"
-				self.answers.append(self.guess)
-				self.solutions -= 1
-				if self.conjugate == True:
-					print "The conjugate is also a new solution."
-					self.answers.append( complex(self.guess.real, 
-						-1*self.guess.imag) )
-				
+		if self.keyword == False:
+			while self.solutions > 0:
+				if self.answers.count(self.guess) == 0:
+					print "That is a new solution"
+					self.answers.append(self.guess)
 					self.solutions -= 1
+						
+					if self.conjugate == True:
+						print "The conjugate is also a new solution."
+						self.answers.append( complex(self.guess.real, 
+							-1*self.guess.imag) )
+	
+						self.solutions -= 1
+					else:
+						print "The conjugate is not unique."
 				else:
-					print "The conjugate is not unique."
-			else:
-				print ("That solution has already been found,\n"
-				"so it will not be added to self.answers.")
-			print "The answers list is {0}".format(self.answers)
+					print ("That solution has already been found,\n"
+					"so it will not be added to self.answers.")
+				print "The answers list is {0}".format(self.answers)
+	
+				print "REPROMPT OUTPUT:\n"
+				print "There are %d solutions left.\n" % self.solutions
+				
+				if self.solutions == 0:
+					break
+				#Calling __init__ resets values.
+				self.__init__(sys.argv)
+				#Call iterate to do the essential operations for Newton's method 
+				#again.
+				self.iterate()
+				#Call Newton's method to work with the newly created data, and call
+				#iterate a thousand more times.
+				self.Newtons_method()
+				self.solution_tester()
+				self.approximate()
+				self.answer_spitter()
 
-			print "REPROMPT OUTPUT:\n"
-			print "There are %d solutions left.\n" % self.solutions
-			
-			if self.solutions == 0:
-				break
-			#Calling __init__ resets values.
-			self.__init__(sys.argv)
-			#Call iterate to do the essential operations for Newton's method 
-			#again.
-			self.iterate()
-			#Call Newton's method to work with the newly created data, and call
-			#iterate a thousand more times.
-			self.Newtons_method()
-			self.solution_tester()
-			self.approximate()
-			self.answer_spitter()
+		else:
+			#The Fundamental Theorem of Algebra cannot be used, so just keeping
+			#running until the user is done.
+			while 1:
+				self.__init__(sys.argv)
+				self.expression_creator()
+				self.special_Newtons_method()
+				self.solution_tester()
+				self.approximate()
+				self.answer_spitter()
 
 
 	def answer_spitter(self):
 		#Default bool value is False.
-		self.conjugate = bool()
+		self.conjugate = False
 		print "ANSWER_SPITTER OUTPUT:\n"
 		#If the imaginary part exists and all the coeffcients are real, 
 		#then the conjugate also exists.
@@ -381,8 +385,8 @@ class Solver:
 		getcontext().prec = 30
 		print "self.guess squared is",(self.guess)**2
 		
-	#If the input entered doesn't correspond to a polynomial, then all of 
-	#the following methods do not apply.
+#If the input entered doesn't correspond to a polynomial, then NONE OF THE
+#FOLLOWING METHODS APPLY.
 	def polynomial_checker(self):
 		print "CHECKER OUTPUT:\n"
 		self.polynomial = bool()
@@ -419,13 +423,12 @@ class Solver:
 
 	#Uses the complex conjugate theorem: if a + b*j is a root, then a - b*j
 	#is also a root.
-	def complex_conjugate(self):
+	def conjugate_tester(self):
 		print "CONJUGATE OUTPUT:\n"
 		#Checks whether all of the coefficients are real, if not, this method
 		# a condition in number_of_roots, and a condition in answer_spitter
 		#doesn't apply.
 		#Default value is 0.
-		self.conjugate_return = int()
 		self.i = 1
 		m = 0
 		self.argcounter = (len(self.args) - 1 )/2
@@ -445,11 +448,9 @@ class Solver:
 
 	def number_of_roots(self):
 		print "ROOT_NUMBER OUTPUT:\n"
-		#self.solutions = max(self.powers)
 		#If reprompt hasn't been called yet.
-		if self.reprompt_return_value ==  1:			
+		if self.reprompt_return_value ==  1 and self.keyword == False:			
 			self.solutions = max(self.powers)
-
 		#Uses the Fundamental Theorem of Algebra to tell the user how many
 		#roots there are.
 		
@@ -472,6 +473,7 @@ class Solver:
 		self.irrational = bool()
 		x = 0
 		i = 1
+		#if it makes it through the for loop, then no coefficients are irrational
 		for coefficient in self.args:
 			self.args[i] = complex(self.args[i])
 			print self.args[i]
@@ -487,8 +489,7 @@ class Solver:
 				if i > len(self.args) -2:
 					break	
 		print "All coeffiecients are rational"
-	
-	#if it makes it through the for loop, then no coefficients are irrational
+
 
 #The arguments 1 2 1 0 give ZeroDivisionError when the guess = 1, but works
 #correctly when the guess = 1j
@@ -496,55 +497,44 @@ class Solver:
 #SOMETIMES WHEN THE GUESS IS CLOSE TO 0 IT GIVES A COMPLEX SOLUTION, LIKE WITH
 #ARGUMENTS 1 0.5 -15 3
 
-#TRY STATEMENT FOR ZERO DIVISION ERRORS
-
 #TODO: arguments 2 3 4 5 6 0.9 give zero division error when the guess isn't complex
 
 
-#TODO: arguments 1 3 4 -9 has a COMPLEX solution PURE IMAGINARY GUESSES MAKE IT
-#GIVE FALSE SOLUTIONS.
-
 #Work on the irrational number methods
-
-
-#TODO: how to find double roots?
 
 solverObject = Solver(sys.argv)
 
 solverObject.setter()
 #All of these lines only get called once; reprompt handles all the subsequent 
 #calls.
-#solverObject.iterate()
 solverObject.power_rule_substitution()
-solverObject.special_differentiation()
-solverObject.Newtons_method()
+if solverObject.keyword == True:
+	solverObject.expression_creator()
+	solverObject.special_Newtons_method()
+else:
+	solverObject.sum_method(solverObject.power_rule_list)
+	solverObject.sum_method(solverObject.list_without_power_rule_operation)
+	solverObject.Newtons_method()
+
 solverObject.solution_tester()
 solverObject.approximate()
 
 #if reprompt hasn't been called yet
 if solverObject.reprompt_return_value == 1:
-	solverObject.polynomial_checker()
-	if solverObject.polynomial == True:
-		solverObject.complex_conjugate()
-		solverObject.number_of_roots()
+	if solverObject.keyword == False:
+		#If there are no keywords, then the polynomial attribute is created.
+		solverObject.polynomial_checker()
+		if solverObject.polynomial == True:
+			solverObject.conjugate_tester()
+			solverObject.number_of_roots()
 		#solverObject.irrational_coefficient_checker()
 solverObject.answer_spitter()
 solverObject.reprompt()
 
 
 
+
 #Useless code, right now:
-"""
-#Return value of -1 means the string wasn't found.
- 
- 
-if ( self.args[i].find("sin") or self.args[i].find("cos") 
-or self.args[i].find("tan") or self.args[i].find("cot")
-or self.args[i].find("csc") or self.args[i].find("sec") 
-or self.args[i].find("ln") or self.args[i].find("log")
-or self.args[i].find("math.e") 
-or self.args[i].find("math.pi") != -1 ):
-"""
 
 
 #Pseudocode
